07680b320e2f4c404cff2fc44b1bdfba09171c42
Jeremy Moore <archtechfl@gmail.com>
2014-04-14 14:52:12 -0400

    commented out jade

--- index.js
+++ index.js
@@ -75,12 +75,14 @@ io.sockets.on('connection', function (socket) {
 
 //Prepare front end template file
 
+/*
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.get("/", function(req, res){
     res.render("page");
 });
+*/
 
  
 app.use(express.static(__dirname + '/public')); 
