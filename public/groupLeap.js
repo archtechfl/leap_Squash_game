8ea15fd337c9ef3dc6f2508418aa7f8d4feba1d0
Jeremy Moore <archtechfl@gmail.com>
2014-04-14 15:26:47 -0400

    Update groupLeap.js

--- public/groupLeap.js
+++ public/groupLeap.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    	var socket = io.connect('http://localhost:3700');
+    	var socket = io.connect('142.55.34.84:3700');
     	var userID = 0;
 	var myClient = 0;
 	var theUserCount = 0;
