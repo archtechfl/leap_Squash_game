999ca729f6597498ff0952af8717973d3719cfa7
Twiddledidlydum <roginska@sheridan.desire2learn.com>
2014-04-14 11:55:42 -0400

    commented out section...
    bottom of doc

--- public/Canvas3D.js
+++ public/Canvas3D.js
@@ -136,3 +136,159 @@ function movement()
 
 
 }
+
+/*
+//global variables
+  // paddle variables
+  var paddleWidth, paddleHeight, paddleDepth, paddleQuality;
+  var paddle1DirY = 0, paddle2DirY = 0, paddleSpeed = 3;
+
+  // paddle variables
+  var paddle1, paddle2;
+
+  // game-related variables
+  var score1 = 0, score2 = 0;
+  // you can change this to any positive whole number
+  var maxScore = 21;
+
+function setup()
+{
+  // update the board to reflect the max score for match win
+  document.getElementById("scoreDiv").innerHTML = "First to " + maxScore + " wins!";
+
+  score1 = 0;
+  score2 = 0;
+
+  // set up all the 3D objects in the scene 
+  createScene();
+
+  // and let's get cracking!
+  draw();
+}
+
+function createScene(){
+  // create the paddle1's material
+  var paddle1Material =
+    new THREE.MeshLambertMaterial(
+    {
+      color: 0x1B32C0;
+    });
+  // create the paddle2's material
+  var paddle2Material =
+    new THREE.MeshLambertMaterial(
+    {
+      color: 0xFF4045;
+    });
+    // // set up the paddle vars
+      paddleWidth = 10;
+      paddleHeight = 30;
+      paddleDepth = 10;
+      paddleQuality = 1;
+
+      paddle1 = new THREE.Mesh(
+
+        new THREE.CubeGeometry(
+            paddleWidth,
+            paddleHeight,
+            paddleDepth,
+            paddleQuality,
+            paddleQuality,
+            paddleQuality),
+          paddle1Material,
+        );
+
+        paddle2 = new THREE.Mesh(
+        new THREE.CubeGeometry(
+            paddleWidth,
+            paddleHeight,
+            paddleDepth,
+            paddleQuality,
+            paddleQuality,
+            paddleQuality),
+          paddle1Material,
+        );
+
+        // set paddles on each side of the table
+        paddle1.position.x = -fieldWidth/2 + paddleWidth;
+        paddle2.position.x = fieldWidth/2 - paddleWidth;
+
+        // lift paddles over playing surface
+        paddle1.position.z = paddleDepth;
+        paddle2.position.z = paddleDepth;
+
+
+
+}
+function draw (){
+
+    paddlePhysics();
+    playerPaddleMovement();
+
+}
+
+function playerPaddleMovement()
+{
+ //paddle + leap 
+}
+
+function paddlePhysics(){
+  if (ball.position.x <= paddle1.position.x + paddleWidth
+  &&  ball.position.x >= paddle1.position.x)
+  {
+    // and if ball is aligned with paddle1 on y plane
+    if (ball.position.y <= paddle1.position.y + paddleHeight/2
+    &&  ball.position.y >= paddle1.position.y - paddleHeight/2)
+    {
+      // and if ball is travelling towards player (-ve direction)
+      if (ballDirX < 0)
+      {
+        matchScoreCheck();
+        // stretch the paddle to indicate a hit
+        paddle1.color = #27A60E;
+        // switch direction of ball travel to create bounce
+        ballDirX = -ballDirX;
+        // we impact ball angle when hitting it
+        // this is not realistic physics, just spices up the gameplay
+        // allows you to 'slice' the ball to beat the opponent
+        ballDirY -= paddle1DirY * 0.7;
+      }
+    }
+  }//paddle1 1 end
+  //opponent logic (same as above with paddle2 instead of paddle1
+  if (ball.position.x <= paddle2.position.x + paddleWidth
+  &&  ball.position.x >= paddle2.position.x)
+  {
+    // and if ball is aligned with paddle1 on y plane
+    if (ball.position.y <= paddle2.position.y + paddleHeight/2
+    &&  ball.position.y >= paddle2.position.y - paddleHeight/2)
+    {
+      // and if ball is travelling towards player (-ve direction)
+      if (ballDirX < 0)
+      {
+        matchScoreCheck();
+        paddle2.color = #27A60E;
+        // switch direction of ball travel to create bounce
+        ballDirX = -ballDirX;
+      }
+    }
+  }//paddle2 end
+}
+
+function matchScoreCheck() {
+  if (score1 >= maxScore)
+  {
+    // stop the ball
+    ballSpeed = 0;
+    // write to the score div
+    document.getElementById("scores").innerHTML = "Player 1 wins!";
+  }
+  // else if opponent has 7 points
+  else if (score2 >= maxScore)
+  {
+    // stop the ball
+    ballSpeed = 0;
+
+    document.getElementById("scores").innerHTML = "Player 2 wins!";
+  }
+}
+*/
