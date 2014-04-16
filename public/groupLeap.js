40423a27128490256dcb70ed41a640bfec39a5af
Jeremy Moore <archtechfl@gmail.com>
2014-04-16 14:16:17 -0400

    Update groupLeap.js

--- public/groupLeap.js
+++ public/groupLeap.js
@@ -16,7 +16,9 @@ window.onload = function() {
 	zSpeed, 
 	ySpeed, 
 	courtWidth, 
-	courtDepth, 
+	courtDepth,
+	courtXStart,
+	courtXEnd,
 	zPosCourt, 
 	yPosCourt, 
 	courtTop,
@@ -50,11 +52,102 @@ window.onload = function() {
 	ballRadius,
 	myPlayer = '';
 	
+	/*Front page animations*/
+	
+	var modal = document.getElementById('window')
+			  , play = document.getElementById('play')
+			  , instruct = document.getElementById('instruct');
+		modal.setAttribute('class','slidein');
+		play.addEventListener('click',prepareStart,false);
+		instruct.addEventListener('click',instructions,false);
+		
+	/*End front page setup*/
+	
+	/*Front page functions*/
+	
+	function instructions(e) {
+			// show game instructions inside modal window
+			var modal = document.getElementById('window')
+			  , h2 = document.createElement('h2')
+			  , title = document.createTextNode('Instructions')
+			  , p = document.createElement('p')
+			  , info = document.createTextNode('Use your hand to control the paddle. First person to 21 hits wins!')
+			  , button = document.createElement('button')
+			  , play = document.createTextNode('Let\'s Play');
+			while (modal.firstChild) {
+				modal.removeChild(modal.firstChild);
+			}
+			h2.appendChild(title);
+			p.appendChild(info);
+			button.appendChild(play);
+			button.addEventListener('click',prepareStart,false);
+			modal.appendChild(h2);
+			modal.appendChild(p);
+			modal.appendChild(button);
+		}
+	function prepareStart(e) {
+			var modal = document.getElementById('window');
+			e.target.removeEventListener('click',prepareStart,false);
+			modal.classList.add('slideout');
+			modal.addEventListener('animationend',waiting,false);
+			modal.addEventListener('oAnimationEnd',waiting,false);
+			modal.addEventListener('webkitAnimationEnd',waiting,false);
+		}
+		
+	function waiting(e) {
+		console.log("waiting for more people");
+		e.target.parentNode.removeChild(e.target);
+		socket.emit('userConnected', { sessionID: myUserID });
+	}
+		
+	function remove() {
+				var three = document.createElement('h3')
+				  , two = document.createElement('h3')
+				  , one = document.createElement('h3');
+				three.innerHTML = '3';
+				three.setAttribute('class','popin');
+				three.addEventListener('animationend',next,false);
+				three.addEventListener('oAnimationEnd',next,false);
+				three.addEventListener('webkitAnimationEnd',next,false);
+				two.innerHTML = '2';
+				one.innerHTML = '1';
+				document.body.appendChild(three);
+				function next(e) {
+					three.parentNode.removeChild(three);
+					two.setAttribute('class','popin');
+					two.addEventListener('animationend',next,false);
+					two.addEventListener('oAnimationEnd',next,false);
+					two.addEventListener('webkitAnimationEnd',next,false);
+					document.body.appendChild(two);
+					function next(e) {
+						two.parentNode.removeChild(two);
+						one.setAttribute('class','popin');
+						one.addEventListener('animationend',next,false);
+						one.addEventListener('oAnimationEnd',next,false);
+						one.addEventListener('webkitAnimationEnd',next,false);
+						document.body.appendChild(one);
+						function next(e) {
+							one.parentNode.removeChild(one);
+							
+							/*Actually start the game*/
+							setInterval(sendData, (1000/15));
+							init();
+							takeALeap();
+							drawCourt();
+							addPaddles();
+							render();
+						}
+					}
+				}
+			}
+		
+	/*End front page functions*/
+	
     socket.on('connect', function () 
     	{
     		myUserID = this.socket.sessionid;
-		console.log("my session id: " + myUserID);
-    		socket.emit('userConnected', { sessionID: myUserID });
+			console.log("my session id: " + myUserID);
+    		//socket.emit('userConnected', { sessionID: myUserID });
     		
     	});
 		
@@ -108,37 +201,17 @@ window.onload = function() {
 			xSpeed = speedArray[0];
 			ySpeed = speedArray[1];
 			zSpeed = speedArray[2];
-			
-			/*
-			//People only start transmitting their info to the server once two players have joined
-			if (userCount > 0){
-				setInterval(sendData, (1000/15));
-				//console.log("Initializing all functions");
-				init();
-				takeALeap();
-				drawCourt();
-				addPaddles();
-				render();
-				
-			}
-			*/
     		
     	});
 		
 		socket.on('startGame', function (data) 
     	{
-    	
-			message = data.response;
+		
+			console.log("starting game");
 			
-			console.log(message);
+			//message = data.response;
 			
-			setInterval(sendData, (1000/15));
-			//console.log("Initializing all functions");
-			init();
-			takeALeap();
-			drawCourt();
-			addPaddles();
-			render();
+			remove();
     		
     	});
     	
@@ -189,18 +262,42 @@ function init(){
 	 	renderer = new THREE.WebGLRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild(renderer.domElement);
-
+		
 		ballRadius = 1.25;
+		
+		//Lighting for the scene, two lights from different sides
+		
+		/*
+		var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+		directionalLight.position.set(-5, 0, 5);
+		scene.add( directionalLight );
+	
+		var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+		directionalLight2.position.set(5, 0, 5);
+		scene.add( directionalLight2 );
+		
+		var directionalLight3 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+		directionalLight3.position.set(0, -5, 50);
+		scene.add( directionalLight3 );
+		
+		var directionalLight4 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+		directionalLight4.position.set(0, 5, 50);
+		scene.add( directionalLight4 );
+		*/
+		
+		var light = new THREE.PointLight( 0xffffff, 1, 400 );
+		light.position.set( 0, 0, 50 );
+		scene.add( light );
 		
 		geometry = new THREE.SphereGeometry( ballRadius, 32, 32 );
 
-		material = new THREE.MeshBasicMaterial( { color: 0xBABABA, wireframe: true} );
-		material_ball = new THREE.MeshBasicMaterial( { color: 0xCC0000, wireframe: false} );
+		material = new THREE.MeshLambertMaterial( { color: 0xBABABA, wireframe: false} );
+		material_ball = new THREE.MeshLambertMaterial( { color: 0xCC0000, wireframe: false} );
 
 		// create the paddle1's material
-		var paddle1Material = new THREE.MeshBasicMaterial({color: 0x1B32C0, wireframe: true});
+		var paddle1Material = new THREE.MeshLambertMaterial({color: 0x1B32C0, wireframe: true});
   		// create the paddle2's material
-		var paddle2Material = new THREE.MeshBasicMaterial({color: 0x521B6B, wireframe: true});
+		var paddle2Material = new THREE.MeshLambertMaterial({color: 0x521B6B, wireframe: true});
 
 		paddleWidth = 10;
 	 	paddleHeight = 8;
@@ -254,14 +351,18 @@ function drawCourt () {
 	//console.log("drawCourt");
 
 	courtWidth = 50;
+	courtXStart = -(courtWidth/2);
+	courtXEnd = (courtWidth/2);
 	courtDepth = courtWidth * 1.7;
 	zPosCourt = 0;
 	courtBottom = -12;
 	yPosCourt = courtBottom;
 	var courtHeight = 24;
 	courtTop = yPosCourt + courtHeight;
-
+	
 	for (i=0; i <= 1; i++){
+	
+		
 		console.log("Create top and bottom");
 		var rectShape = new THREE.Shape();
 		rectShape.moveTo( 0,0 );
@@ -271,19 +372,43 @@ function drawCourt () {
 		rectShape.lineTo( 0, 0 );
 
 		var rectGeom = new THREE.ShapeGeometry( rectShape );
-		var rectMesh = new THREE.Mesh( rectGeom, material ) ;	
+		var rectMesh = new THREE.Mesh( rectGeom, material );	
 
 		rectMesh.position.set( -(courtWidth/2), yPosCourt, zPosCourt );
 		rectMesh.rotation.set( ((2 * Math.PI) * -0.25), 0, 0);
 
 		court.add( rectMesh );
 		yPosCourt += courtHeight;
+		
+	}
+	
+	for (i=0; i <= 1; i++){
+		
+		console.log("Create left and right");
+		var rectShape = new THREE.Shape();
+		rectShape.moveTo( 0,0 );
+		rectShape.lineTo( 0, courtDepth );
+		rectShape.lineTo( courtHeight, courtDepth );
+		rectShape.lineTo( courtHeight, 0 );
+		rectShape.lineTo( 0, 0 );
+	
+		var rectSide = new THREE.ShapeGeometry( rectShape );
+		var rectSideMesh = new THREE.Mesh( rectSide, material );
+	
+		rectSideMesh.position.set( courtXStart, courtBottom, zPosCourt );
+		rectSideMesh.rotation.set( ((2 * Math.PI) * -0.25), ((2 * Math.PI) * -0.25), 0);
+	
+		court.add( rectSideMesh );
+		courtXStart += courtWidth;
+		
 	}
 
 	scene.add( court );
 
 	camera.position.z = 20;
 	camera.position.y = 0;
+	camera.position.x = 0;
+	
 
 	}//end of draw court
 
@@ -442,6 +567,7 @@ function movePaddle() {
 	var vAF = 1.7;
 	
 	if (myPlayer == 'player1'){
+	
 		paddle1.position.x = xPos * 0.3;
 		paddle1.position.y = (yPos * 0.11) + (courtBottom * vAF);
 		paddle1.position.z = (zPos - 100) * 0.1;
