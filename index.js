1aa428a935c66662725a3b90b232d4740912c0e9
Jeremy Moore <archtechfl@gmail.com>
2014-04-16 14:15:44 -0400

    integrated intro screens

--- index.js
+++ index.js
@@ -51,66 +51,53 @@ io.sockets.on('connection', function (socket) {
 	    
 	    	socket.on('userConnected', function (data) {
 	    
-			clientID = Math.floor(Math.random() * 1e6);
-			socket.clientID = clientID;
-			
-			//Session ID to client ID mapping
-			lastID = data.sessionID;
-			userMap[clientID] = data.sessionID;
-			
-			keyTemp = [];
-			if (numUsers > 0){
-				for (var key in userMap){
-					keyTemp.push(key);
-					console.log(keyTemp);
+				clientID = Math.floor(Math.random() * 1e6);
+				socket.clientID = clientID;
+				
+				//Session ID to client ID mapping
+				lastID = data.sessionID;
+				userMap[clientID] = data.sessionID;
+				
+				keyTemp = [];
+				if (numUsers > 0){
+					for (var key in userMap){
+						keyTemp.push(key);
+						console.log(keyTemp);
+					}
+					
+					opponentMap[keyTemp[0]] = keyTemp[1];
+					opponentMap[keyTemp[1]] = keyTemp[0];
+					
 				}
-				
-				opponentMap[keyTemp[0]] = keyTemp[1];
-				opponentMap[keyTemp[1]] = keyTemp[0];
-				//console.log(keyTemp[1]);
-				//console.log(opponentMap[keyTemp[0]]);
-				//console.log(keyTemp[0]);
-				//console.log(opponentMap[keyTemp[1]]);
-				
-			}
-				
-			console.log("User map testing: " + userMap[clientID]);
-		
-		    		if (numUsers < 1){
-		    			io.sockets.emit('loginResponse', {response: "First person joined", map: userMap, myClientID: clientID, userCount: numUsers, speedArray: speedArrayServer, player:playerNames[0]});
-		    		} else if (numUsers > 0){
-		    			io.sockets.socket(lastID).emit('loginResponse', {response: "Second person joined", map: userMap, myClientID: clientID, userCount: numUsers, speedArray: speedArrayServer, player:playerNames[1]});
-						io.sockets.emit('startGame', {response: "Game has begun"});
-					console.log("Maximum number of users reached");
-		    		} else {
-		    			console.log("Nothing to do");
-		    		}
+					
+				console.log("User map testing: " + userMap[clientID]);
 			
-	    		numUsers++;
-			io.sockets.emit('userCountUpdate', {count: numUsers});
-			
-	   		console.log("Numbers of users: " + numUsers);
-			
-		});
+						if (numUsers < 1){
+							io.sockets.emit('loginResponse', {response: "First person joined", map: userMap, myClientID: clientID, userCount: numUsers, speedArray: speedArrayServer, player:playerNames[0]});
+						} else if (numUsers > 0){
+							io.sockets.socket(lastID).emit('loginResponse', {response: "Second person joined", map: userMap, myClientID: clientID, userCount: numUsers, speedArray: speedArrayServer, player:playerNames[1]});
+							io.sockets.emit('startGame', {response: "Game has begun"});
+						console.log("Maximum number of users reached");
+						} else {
+							console.log("Nothing to do");
+						}
+				
+					numUsers++;
+				io.sockets.emit('userCountUpdate', {count: numUsers});
+				
+				console.log("Numbers of users: " + numUsers);
+				
+			});
 	
-	socket.on('leapData', function (data) {
+		socket.on('leapData', function (data) {
 		
 			var dataHolder = {};
 			console.log("Incoming ID: " + data.id + ' incoming coordinates: ' + data.coord);
 			
 			dataHolder[data.id] = data.coord;
 			
-			/* Debug */
-			/*
-			for (var key in dataHolder){
-				console.log(key);
-				console.log("Temp data: " + dataHolder[key]);
-			}
-			*/
-			
 			//Only transmit when more than one person has joined
 			if (numUsers > 1){
-				//io.sockets.emit('sendOutData', data);
 				for (var key in opponentMap){
 					var ID_OPPOS = opponentMap[data.id];
 					destination = userMap[ID_OPPOS];
@@ -120,34 +107,22 @@ io.sockets.on('connection', function (socket) {
 			
 		});
 	
-	socket.on('disconnect', function(data) {
+		socket.on('disconnect', function(data) {
+			
+				console.log('Someone disconnected!');
+			  
+			console.log("This person disconnected: " + this.clientID);
+			delete userMap[this.clientID];
 		
-	        console.log('Someone disconnected!');
-		  
-		console.log("This person disconnected: " + this.clientID);
-		delete userMap[this.clientID];
-	
-	        numUsers -= 1;
-	
-		console.log("New number of users: " + numUsers);
-		io.sockets.emit('userCountUpdate', {count: numUsers});
-		  
-		});
+				numUsers -= 1;
+		
+			console.log("New number of users: " + numUsers);
+			io.sockets.emit('userCountUpdate', {count: numUsers});
+			  
+			});
 
 });
 
-//Prepare front end template file
-
-/*
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-    res.render("page");
-});
-*/
-
- 
 app.use(express.static(__dirname + '/public')); 
 
 console.log("Listening on port " + port);
